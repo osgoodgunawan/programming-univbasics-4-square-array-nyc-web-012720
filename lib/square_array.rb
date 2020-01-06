63e3c2024b75138545fd78cba8e66606ac57517d
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-06 04:37:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
  new_array=[]
  counter=0
   while counter<array.size do
-    new_array.push(array[counter]*array[counter])
+    new_array.push(array[counter]**2)
     counter+=1
   end
    new_array
